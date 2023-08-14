6b3ee53b68de4b74d14877776ac4dcf68ac650b4
Tyler Termini <tntermini@gmail.com>
2023-08-14 10:14:43 -0700

    add newlines in shell script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "===================  Topic classification starting... ======================="
-
+echo ""
 cd /usr/src/app
 . .venv/bin/activate
 python3 topic_classification.py
-
+echo ""
 echo "===================  Topic classification complete. ======================="

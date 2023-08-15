076e717ae464bf66a756a18ac30b09ac0e791931
Tyler Termini <tntermini@gmail.com>
2023-08-15 12:56:35 -0700

    (wip) (working) topic classification, summarization, all working. Job logging working, including collecting incomplete summaries and rejected topics for later analysis.

--- topic_classification.py
+++ topic_classification.py
@@ -1,8 +1,10 @@
 import re
 import time
-import datetime
 import os
+from typing import List
 from data_stores.mongodb import thoughts_db
+from bson.objectid import ObjectId
+import utils
 import openai
 from anthropic import (
     Anthropic,
@@ -64,6 +66,11 @@ def generate_topics(content: str, title: str):
         Feel free to use less than three topics if you can't find three topics from the list that are a good fit.
         If you pick a topic that is two words or more, make sure every word is capitalized (not just the first word).
 
+        Here are some notes regarding topics which are identical but might be called different names: 
+        - If you choose "Mathematics" as a topic, please just call it "Math".
+        - If you choose "Health" as a topic, please call it "Medicine or Health."
+        - If you choose "Film" as a topic, please just call it "Culture".
+
         Article title: {title}
         Article summary: {content}
     """
@@ -90,13 +97,15 @@ def generate_topics(content: str, title: str):
     response = completion.choices[0].message
 
     parsed_topics = []
+    rejected_topics = []
     for topic in response.content.split("\n"):
         stripped_topic = topic.replace("-", "").strip()
         if stripped_topic in master_topics:
             parsed_topics.append(stripped_topic)
         else:
-            print("Omitted this topic: ", stripped_topic)
-    return parsed_topics
+            print("Rejected this topic: ", stripped_topic)
+            rejected_topics.append(stripped_topic)
+    return {"accepted_topics": parsed_topics, "rejected_topics": rejected_topics}
 
 
 def summary_seems_incomplete(summary: str):
@@ -107,19 +116,24 @@ def summary_seems_incomplete(summary: str):
 
 
 # TODO: LATER: something more robust down the road...possibly tapping into our existing rules db collection
-def thought_should_be_processed(content, title):
+def thought_should_be_processed(thought, parsedContent):
     """
     Determine if thought should be processed according to simple filter rules.
     """
-    if "assorted links" in title:
+    # Tyler Cowen
+    if "assorted links" in thought["title"]:
         return False
-    if len(content) < 450:
+    if "appeared first on Marginal REVOLUTION" in parsedContent[-250:]:
         return False
-    if "read more" in content[-250:]:
+    # Content too short
+    if len(parsedContent) < 450:
         return False
-    if "appeared first on Marginal REVOLUTION" in content[-250:]:
+    # Content is already truncated
+    if "read more" in parsedContent[-250:]:
         return False
-
+    # Ignore content from voice "Public"
+    if ObjectId("64505e4c509cac9a8e7e226d") in thought["voicesInContent"]:
+        return False
     return True
 
 
@@ -127,16 +141,16 @@ def store_transcript(thought_pointer, transcript):
     # thought_collection = thought_pointer["collection"]
     thought_id = thought_pointer["_id"]
 
-    # updateOp = thoughts_db[thought_collection].update_one(
+    # update_op = thoughts_db[thought_collection].update_one(
     #     {"_id": thought_id}, {"$set": {"content_transcript": transcript}}
     # )
 
     # TEST
-    updateOp = thoughts_db["test_topic_classification"].update_one(
+    update_op = thoughts_db["test_topic_classification"].update_one(
         {"_id": thought_id}, {"$set": {"content_transcript": transcript}}
     )
 
-    return updateOp.modified_count
+    return update_op.modified_count
 
 
 def collect_thoughts_for_classification(limit=1000):
@@ -147,8 +161,8 @@ def collect_thoughts_for_classification(limit=1000):
     # for thought in thoughts_db[collection].find(
     for thought in thoughts_db["test_topic_classification"].find(
         {
-            "collection": "yt",
             "valuable": True,
+            "reviewed": True,
             "title": {"$ne": None},
             # TODO: LATER: Do we want to filter for thoughts that have already been topic-classified?
             # If so, remove this
@@ -166,22 +180,23 @@ def collect_thoughts_for_classification(limit=1000):
             "vid": 1,
             "title": 1,
             "content_text": 1,
+            "voicesInContent": 1,
         },
         limit=limit,
     ):
-        content = ""
+        parsedContent = ""
         if thought.get("vid") != None:
             try:
                 loader = YoutubeLoader.from_youtube_url(thought.get("url"))
                 transcript = loader.load()
                 if len(transcript) > 0:
-                    content = transcript[0].page_content
+                    parsedContent = transcript[0].page_content
                     documents_modified = store_transcript(
                         {
                             # "collection": "yt",
                             "_id": thought["_id"]
                         },
-                        content,
+                        parsedContent,
                     )
                     if documents_modified == 0:
                         # throw some warning about "transcript not saved"
@@ -190,18 +205,19 @@ def collect_thoughts_for_classification(limit=1000):
                 print(f"No transcript found for youtube video {thought['url']}")
                 continue
         elif thought.get("content_text") != None:
-            content = thought["content_text"]
+            parsedContent = thought["content_text"]
         else:
             continue
 
+        # Only the fields necessary for performing summarization/topic classification.
         lean_thought_for_processing = {
             # "collection": collection,
             # "collection": thought["collection"],
             "_id": thought["_id"],
-            "content": content,
+            "content": parsedContent,
             "title": thought["title"],
         }
-        if thought_should_be_processed(content, thought["title"]):
+        if thought_should_be_processed(thought, parsedContent):
             thoughts_to_classify.append(lean_thought_for_processing)
         else:
             thoughts_to_skip.append(lean_thought_for_processing)
@@ -214,44 +230,60 @@ def collect_thoughts_for_classification(limit=1000):
     }
 
 
-def topic_classification(limit=1000):
+def main(limit=1000):
+    # TODO: LATER: Clean up this function from all this job mgmt logic
+    job_id = utils.create_job()
+
     classification_candidates = collect_thoughts_for_classification(limit)
-    print(
-        "thoughts to classify #: ",
-        classification_candidates["thoughts_to_classify_count"],
-    )
-    print("thoughts to skip #: ", classification_candidates["thoughts_to_skip_count"])
-    thoughts_classified = []
+    thoughts_classified: List[ObjectId] = []
+    all_incomplete_summaries = []
+    all_rejected_topics = {}
 
     for thought in classification_candidates["thoughts_to_classify"]:
-        now = datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
+        now = utils.get_now()
 
         generated_summary = generate_summary(thought["content"], thought["title"])
-        if summary_seems_incomplete:
-            # TODO: do something, like add to a job metadata in cicero_jobs.ml_jobs (total # of "seems incomplete" with embedded examples)
-            pass
+        if summary_seems_incomplete(generated_summary):
+            all_incomplete_summaries.append(
+                {
+                    "_id": thought["_id"],
+                    "title": thought["title"],
+                    "generated_summary": generated_summary,
+                }
+            )
 
         generated_topics = generate_topics(generated_summary, thought["title"])
 
-        updateOp = thoughts_db["test_topic_classification"].update_one(
+        # compile all rejected topics for later analysis. Don't need to include
+        # reference to the original thought, because we'll write both accepted and rejected topics
+        # to the thought in the final write operation (see below)
+        for topic in generated_topics["rejected_topics"]:
+            if hasattr(all_rejected_topics, topic):
+                all_rejected_topics[topic] += 1
+            else:
+                all_rejected_topics[topic] = 1
+
+        workflows_completed = [
+            {
+                "name": "summarization",
+                "model": "claude-instant-v1-100k",
+                "last_performed": now,
+            },
+            {
+                "name": "topic_classification",
+                "model": "chatgpt-3.5-turbo",
+                "last_performed": now,
+            },
+        ]
+
+        update_op = thoughts_db["test_topic_classification"].update_one(
             {"_id": thought["_id"]},
             {
                 "$set": {
                     "llm_generated_summary": generated_summary,
-                    "llm_generated_topics": generated_topics,
+                    "llm_generated_topics": generated_topics,  # This includes both accepted and rejected topics.
                     "llm_processing_metadata": {
-                        "workflows_completed": [
-                            {
-                                "name": "summarization",
-                                "model": "claude-instant-v1-100k",
-                                "last_performed": now,
-                            },
-                            {
-                                "name": "topic_classification",
-                                "model": "chatgpt-3.5-turbo",
-                                "last_performed": now,
-                            },
-                        ],
+                        "workflows_completed": workflows_completed,
                         "fields_written": [
                             "llm_generated_summary",
                             "llm_generated_topics",
@@ -261,8 +293,23 @@ def topic_classification(limit=1000):
             },
         )
 
-        if updateOp.modified_count == 1:
+        if update_op.modified_count == 1:
             thoughts_classified.append(thought["_id"])
+
+    utils.update_job(
+        job_id,
+        {
+            "status": "complete",
+            "last_updated": utils.get_now(),
+            "workflows_completed": workflows_completed,
+            "thoughts_updated": thoughts_classified,
+            "thoughts_updated_count": len(thoughts_classified),
+            "job_metadata": {
+                "incomplete_summaries": all_incomplete_summaries,
+                "rejected_topics": all_rejected_topics,
+            },
+        },
+    )
 
     return dict(
         {
@@ -273,8 +320,8 @@ def topic_classification(limit=1000):
 
 if __name__ == "__main__":
     tic = time.perf_counter()
-    x = topic_classification(limit=10)
-    # x = topic_classification()
+    x = main(limit=10)
+    # x = main()
     toc = time.perf_counter()
     pprint(x)
     print(f"Time elapsed: {toc-tic:0.4f}")

34e243ee7efc32397a47c8eff73091526c4b0d23
Tyler Termini <tntermini@gmail.com>
2023-08-16 15:54:19 -0700

    use venv in docker container

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /usr/src/app
 
 COPY requirements.txt ./
 
+RUN python3 -m venv .venv \
+    && . .venv/bin/activate
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
--- start.sh
+++ start.sh
@@ -2,8 +2,7 @@
 echo "===================  Topic classification starting... ======================="
 echo ""
 cd /usr/src/app
-# local only:
-# . .venv/bin/activate
+echo "query limit per collection set to: ${PER_COLLECTION_LIMIT}"
 python3 topic_classification.py $PER_COLLECTION_LIMIT
 echo ""
 echo "===================  Topic classification complete. ========================="

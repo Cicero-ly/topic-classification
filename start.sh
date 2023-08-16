5cac64f096a9cc75cfe85d0332301b5a6239dba2
Tyler Termini <tntermini@gmail.com>
2023-08-16 16:34:17 -0700

    Part 2 of the previous commit (standardize the "per collection find limit" variable name

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 echo "===================  Topic classification starting... ======================="
 echo ""
 cd /usr/src/app
-python3 topic_classification.py $PER_COLLECTION_LIMIT
+python3 topic_classification.py $SINGLE_COLLECTION_FIND_LIMIT
 echo ""
 echo "===================  Topic classification complete. ========================="

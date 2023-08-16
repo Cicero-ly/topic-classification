c55afa7286ee2c0bc03f7b557ae460c8107773e1
Tyler Termini <tntermini@gmail.com>
2023-08-15 19:48:45 -0700

    Comment out dev code in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 echo "===================  Topic classification starting... ======================="
 echo ""
 cd /usr/src/app
-. .venv/bin/activate
-python3 topic_classification.py
+# local only:
+# . .venv/bin/activate
+python3 topic_classification.py $PER_COLLECTION_LIMIT
 echo ""
 echo "===================  Topic classification complete. ========================="

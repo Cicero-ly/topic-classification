38f5ee1e85b702230c12791f48295e8460a5eaea
Tyler Termini <tntermini@gmail.com>
2023-08-17 11:17:15 -0700

    comment out venv line in start.sh (for local testing only)

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 echo "===================  Topic classification starting... ======================="
 echo ""
 cd /usr/src/app
+# local testing only:
+# . .venv/bin/activate
 python3 topic_classification.py
 echo ""
 echo "===================  Topic classification complete. ========================="

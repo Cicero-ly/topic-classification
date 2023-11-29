8ca3f7898e38881e3c5af90df50321f1b217b848
Tyler Termini <tntermini@gmail.com>
2023-11-28 17:18:04 -0800

    clean up start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+#
 set -e
+cd /usr/src/app
+
 echo "===================  Topic classification starting... ======================="
 echo ""
-cd /usr/src/app
 python3 topic_classification/main.py
 echo ""
 echo "===================  Topic classification complete. ========================="
-set -e
+
 echo "===================  Rung classification starting... ======================="
 echo ""
 python3 rung_classification/main.py
